c2fc70ff17abda5ca6f0465dab0eaea01a81b58b
Giordano Cardillo <giordano.cardillo@spindox.it>
2022-03-21 16:56:57 +0100

    Disabled leave alert

--- controller/docker-entrypoint.sh
+++ controller/docker-entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 nohup /usr/sbin/sshd -D </dev/null >/dev/null 2>&1 &
-nohup /usr/bin/ttyd -p 9090 -d 0 bash </dev/null >/dev/null 2>&1
+nohup /usr/bin/ttyd -p 9090 -d 0 -t disableLeaveAlert=true bash </dev/null >/dev/null 2>&1
